bbe8bb5f57b25eea2bfa1cf5e86e22e4d5da8ee1
Dominika Repcikova <dominika.repcikova@uni.lu>
2024-08-09 11:12:59 +0200

    Updates

---
+++ static/js/main.90fa526a.chunk.js
@@ -0,0 +1,9 @@
+(this["webpackJsonpbook-tracker"]=this["webpackJsonpbook-tracker"]||[]).push([[0],{15:function(e,t,n){},21:function(e,t,n){"use strict";n.r(t);var c=n(0),o=n.n(c),r=n(6),i=n.n(r),a=(n(15),n(7)),s=n(1);const h=a.a.div`
+  background-image: url(${"/book-tracker"}/bookshelf.jpg);
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;var b=function(){return Object(s.jsx)(h,{children:Object(s.jsx)("h1",{children:"Welcome to the Book Tracker"})})};var k=e=>{e&&e instanceof Function&&n.e(3).then(n.bind(null,22)).then((t=>{let{getCLS:n,getFID:c,getFCP:o,getLCP:r,getTTFB:i}=t;n(e),c(e),o(e),r(e),i(e)}))};i.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(b,{})}),document.getElementById("root")),k()}},[[21,1,2]]]);
+//# sourceMappingURL=main.90fa526a.chunk.js.map
